263726892a1c3b6cd7e1cf948ad5850916fad838
Kazuya Iriguchi <kazuya.iriguchi.j6k@jp.denso.com>
2025-03-04 14:59:41 +0900

    fix: update Azure OpenAI configuration to use environment variables and refine message content

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 import os
-
 from openai import AzureOpenAI
 
 # endpoint of westus
-azure_endpoint = "https://ai-robot-openai.openai.azure.com"
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 api_key = os.getenv("AZURE_OPENAI_API_KEY")
 api_version = os.getenv("AZURE_OPENAI_VERSION")
-model = "gpt-4-filterless"
+model = "gpt-4o"
 
 client = AzureOpenAI(
     azure_endpoint=azure_endpoint, api_key=api_key, api_version=api_version
 )
 
-messages = [{"role": "user", "content": "Perfumeについて3行以内で熱く語って"}]
+messages = [{"role": "user", "content": "Perfumeという日本のアーティストについて、あ～ちゃんの口調で3行以内で熱く語って"}]
 
 response = client.chat.completions.create(messages=messages, model=model)
 
+print(f"{messages=}")
 print(f"AOAI response: {response.model_dump_json(indent=4)}")
